77fe23f48ad0c0bee9e75ce3ca0364940a88e27b
Joonas Kaartti <joonas.kaartti@gmail.com>
2023-09-27 13:15:39 +0300

    anecdotes are fetched from the backend (6.20)

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
@@ -7,13 +9,32 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
+  /*const anecdotes = [
     {
       "content": "If it hurts, do it more often",
       "id": "47145",
       "votes": 0
     },
-  ]
+  ]*/
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => {
+      return axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+    },
+    refetchOnWindowFocus: false,
+    retry: 1
+  })
+
+  if (result.isLoading) {
+    return <span>loading data...</span>
+  }
+
+  if (result.isError) {
+    return <span>anecdote service not available due to problems in server</span>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
